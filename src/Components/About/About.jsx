a7c10b98e4ab4b6b33a2f53fdd843ce9bf2c307e
JaiThakur10 <dev10jai@gmail.com>
2024-03-15 21:44:28 +0530

    about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,31 +2,99 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div className="py-16 bg-white">
-            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                    <div className="md:5/12 lg:w-5/12">
-                        <img
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEzPJKToIUBF-61Sn87mw_7jcke1-N_lyutA&usqp=CAU"
-                            alt="image"
-                        />
-                    </div>
-                    <div className="md:7/12 lg:w-6/12">
-                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                            React development is carried out by passionate developers
-                        </h2>
-                        <p className="mt-6 text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
-                        </p>
-                        <p className="mt-4 text-gray-600">
-                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
-                        </p>
-                    </div>
+        <div className="flex flex-row justify-center w-full">
+          <div className="flex flex-row justify-between w-full max-w-[1290px]">
+            <div className="flex flex-col items-center justify-start w-[47%] gap-[50px]">
+              <Heading size="xl" as="h2" className="tracking-[-0.50px]">
+                OUR STRENGTHS
+              </Heading>
+              <div className="justify-center w-full gap-[50px] grid-cols-2 grid min-h-[auto]">
+                <div className="flex flex-col items-start justify-start w-full gap-10">
+                  <Img
+                    src="images/img_icon.svg"
+                    alt="fast_shipping"
+                    className="h-[60px]"
+                  />
+                  <div className="flex flex-col items-start justify-start w-full gap-[9px]">
+                    <Heading as="h3" className="tracking-[-0.50px]">
+                      Efficient production
+                    </Heading>
+                    <Text
+                      as="p"
+                      className="!text-gray-500 tracking-[-0.50px] leading-[25px]"
+                    >
+                      Every order placed right now is ready and dispatched in
+                      less than 2 days. No waiting for you is our rule.{" "}
+                    </Text>
+                  </div>
                 </div>
+                <div className="flex flex-col items-start justify-start w-full gap-10">
+                  <Img
+                    src="images/img_icon_gray_50_01.svg"
+                    alt="icon_one"
+                    className="h-[60px]"
+                  />
+                  <div className="flex flex-col items-start justify-start w-full gap-[9px]">
+                    <Heading as="h4" className="tracking-[-0.50px]">
+                      Everything under one roof
+                    </Heading>
+                    <Text
+                      as="p"
+                      className="!text-gray-500 tracking-[-0.50px] leading-[25px]"
+                    >
+                      From cleaning to grinding the spices to packing them,
+                      everthing is done under one roof. No travelling of the
+                      spices making them as pure as they can be.{" "}
+                    </Text>
+                  </div>
+                </div>
+                <div className="flex flex-col items-start justify-start w-full gap-10">
+                  <Img
+                    src="images/img_icon_gray_50_01_60x63.svg"
+                    alt="icon_one"
+                    className="h-[60px]"
+                  />
+                  <div className="flex flex-col items-start justify-start w-full pt-0.5 gap-2">
+                    <Heading as="h5" className="tracking-[-0.50px]">
+                      Making the best
+                    </Heading>
+                    <Text
+                      as="p"
+                      className="!text-gray-500 tracking-[-0.50px] leading-[25px]"
+                    >
+                      Chillies from Khammam, Turmeric from Nizamabad or talking
+                      about Coriander from Ramganj. We get raw materials from
+                      the best places in the country, making the best product.{" "}
+                    </Text>
+                  </div>
+                </div>
+                <div className="flex flex-col items-start justify-start w-full gap-10">
+                  <Img
+                    src="images/img_icon_60x63.svg"
+                    alt="icon_one"
+                    className="h-[60px]"
+                  />
+                  <div className="flex flex-col items-start justify-start w-full gap-2.5">
+                    <Heading as="h6" className="tracking-[-0.50px]">
+                      Location
+                    </Heading>
+                    <Text
+                      as="p"
+                      className="!text-gray-500 tracking-[-0.50px] leading-[25px]"
+                    >
+                      You can find us in J&K, Punjab, Hayana, HP, Delhi,
+                      Uttrakhand, UP, Pan India{" "}
+                    </Text>
+                  </div>
+                </div>
+              </div>
             </div>
+            <Img
+              src="images/blend.png"
+              alt="image_one"
+              className="w-[47%] object-cover"
+            />
+          </div>
         </div>
     );
 }
