d314ff096cc7c3c7e76deeed03dd43fb20dec4a7
JaiThakur10 <dev10jai@gmail.com>
2024-03-15 21:45:33 +0530

    home: ig

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,7 +10,6 @@ import HomepageCardproduct from "../HomepageCardproduct/HomepageCardproduct";
 import { Link } from "react-router-dom";
 
 export default function HomepagePage() {
-
   return (
     <>
       <Helmet>
@@ -22,7 +21,7 @@ export default function HomepagePage() {
       </Helmet>
       <div className="flex flex-col items-center justify-start w-full gap-[99px] bg-yellow-50">
         <div className="flex flex-col items-center justify-start w-full max-h-full">
-        <div className="flex flex-row justify-center w-full px-14 py-[75px] bg-orange-50">
+          <div className="flex flex-row justify-center w-full px-14 py-[75px] bg-orange-50">
             <div className="flex flex-row justify-between items-center w-full my-[5px] max-w-[1290px]">
               <div className="flex flex-col items-start justify-start w-[48%] gap-[30px]">
                 <div className="flex flex-col items-start justify-start w-full gap-[26px]">
@@ -52,28 +51,49 @@ export default function HomepagePage() {
                 </div>
               </div>
               <div className="carousel carousel-center max-w-2xl p-4 space-x-7 bg-organe-50 ">
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box " />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="rounded-box" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="rounded-box" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="rounded-box" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="rounded-box" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="rounded-box" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="rounded-box" />
-  </div>
-</div>
+                <div className="carousel-item">
+                  <img
+                    src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+                    className="rounded-box "
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+                    className="rounded-box"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -293,100 +313,14 @@ export default function HomepagePage() {
         <div className="flex flex-row justify-center w-full">
           <div className="flex flex-col items-center justify-start w-full gap-[67px] max-w-[1290px]"></div>
         </div>
-        <div className="flex flex-row justify-center w-full">
-          <div className="flex flex-row justify-between w-full max-w-[1290px]">
-            <div className="flex flex-col items-center justify-start w-[47%] gap-[50px]">
-              <Heading size="xl" as="h2" className="tracking-[-0.50px]">
-                OUR STRENGTHS
+        <Heading
+                size="xl"
+                as="h2"
+                className="tracking-[-0.50px] text-center !font-semibold"
+              >
+                Instagram Posts
               </Heading>
-              <div className="justify-center w-full gap-[50px] grid-cols-2 grid min-h-[auto]">
-                <div className="flex flex-col items-start justify-start w-full gap-10">
-                  <Img
-                    src="images/img_icon.svg"
-                    alt="fast_shipping"
-                    className="h-[60px]"
-                  />
-                  <div className="flex flex-col items-start justify-start w-full gap-[9px]">
-                    <Heading as="h3" className="tracking-[-0.50px]">
-                      Efficient production
-                    </Heading>
-                    <Text
-                      as="p"
-                      className="!text-gray-500 tracking-[-0.50px] leading-[25px]"
-                    >
-                      Every order placed right now is ready and dispatched in
-                      less than 2 days. No waiting for you is our rule.{" "}
-                    </Text>
-                  </div>
-                </div>
-                <div className="flex flex-col items-start justify-start w-full gap-10">
-                  <Img
-                    src="images/img_icon_gray_50_01.svg"
-                    alt="icon_one"
-                    className="h-[60px]"
-                  />
-                  <div className="flex flex-col items-start justify-start w-full gap-[9px]">
-                    <Heading as="h4" className="tracking-[-0.50px]">
-                      Everything under one roof
-                    </Heading>
-                    <Text
-                      as="p"
-                      className="!text-gray-500 tracking-[-0.50px] leading-[25px]"
-                    >
-                      From cleaning to grinding the spices to packing them,
-                      everthing is done under one roof. No travelling of the
-                      spices making them as pure as they can be.{" "}
-                    </Text>
-                  </div>
-                </div>
-                <div className="flex flex-col items-start justify-start w-full gap-10">
-                  <Img
-                    src="images/img_icon_gray_50_01_60x63.svg"
-                    alt="icon_one"
-                    className="h-[60px]"
-                  />
-                  <div className="flex flex-col items-start justify-start w-full pt-0.5 gap-2">
-                    <Heading as="h5" className="tracking-[-0.50px]">
-                      Making the best
-                    </Heading>
-                    <Text
-                      as="p"
-                      className="!text-gray-500 tracking-[-0.50px] leading-[25px]"
-                    >
-                      Chillies from Khammam, Turmeric from Nizamabad or talking
-                      about Coriander from Ramganj. We get raw materials from
-                      the best places in the country, making the best product.{" "}
-                    </Text>
-                  </div>
-                </div>
-                <div className="flex flex-col items-start justify-start w-full gap-10">
-                  <Img
-                    src="images/img_icon_60x63.svg"
-                    alt="icon_one"
-                    className="h-[60px]"
-                  />
-                  <div className="flex flex-col items-start justify-start w-full gap-2.5">
-                    <Heading as="h6" className="tracking-[-0.50px]">
-                      Location
-                    </Heading>
-                    <Text
-                      as="p"
-                      className="!text-gray-500 tracking-[-0.50px] leading-[25px]"
-                    >
-                      You can find us in J&K, Punjab, Hayana, HP, Delhi,
-                      Uttrakhand, UP, Pan India{" "}
-                    </Text>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <Img
-              src="images/blend.png"
-              alt="image_one"
-              className="w-[47%] object-cover"
-            />
-          </div>
-        </div>
+        
 
         <div className="flex flex-row justify-center w-full">
           <div className="flex flex-col items-center justify-start w-full gap-[50px] max-w-[1290px]">
