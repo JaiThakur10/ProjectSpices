663a62549e1b31975e564ad36388a09a48d5e425
Dineshwar420 <dineshwar420@gmail.com>
2024-03-16 18:35:32 +0530

    navbar updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -117,6 +117,7 @@ export default function Header({ ...props }) {
 
       </div>
 
+ {/* function for responsive design  */}
       {isOpen && (
         <div className="md:hidden" id="mobile-menu">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
@@ -140,7 +141,6 @@ export default function Header({ ...props }) {
               Blog
             </NavLink>
 
-<<<<<<< HEAD
             <NavLink
               to="/about"
               className={({ isActive }) =>
@@ -160,14 +160,6 @@ export default function Header({ ...props }) {
               Contact
             </NavLink>
 
-            <NavLink
-              to="/store"
-              className={({ isActive }) =>
-                `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-black-900"
-                }  font-rubik text-4xl    hover:text-orange-700 `
-              }
-            >
-            </NavLink>
 
             <div className="dropdown dropdown-hover  ">
               <div className="tabIndex={0} block py-2 pr-4 duration-200 font-rubik text-4xl cursor-pointer hover:text-orange-700 "
@@ -182,14 +174,14 @@ export default function Header({ ...props }) {
             <NavLink
               to="https://www.flipkart.com/search?q=ypgold%20masala&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
               className={({ isActive }) =>
-                `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-black-900"
+                `block py-0 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-black-900"
                 }  font-rubik text-4xl   hover:text-orange-700 `
               }
             >
               <Img
-                src="images/img_list_black_900.svg"
+                src="./images/Distribution.png"
                 alt="list_one"
-                className="h-6 w-6 "
+                className="w-14 h-16 "
               />
             </NavLink>
             <NavLink
@@ -199,49 +191,6 @@ export default function Header({ ...props }) {
                 }  font-rubik text-4xl   hover:text-orange-700 `
               }
             >
-=======
-          <NavLink
-            to="/store"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 ${
-                isActive ? "text-orange-700" : "text-black-900"
-              }  font-rubik text-4xl   lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-            }
-          >
-            <div className="dropdown dropdown-hover">
-  <div tabIndex={0} role="button" >Buy</div>
-  <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow   rounded-box w-52 bg-orange-50">
-    <li><NavLink to="https://www.flipkart.com/search?q=ypgold%20masala&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"><img className="w-30 h-14" src="images/flipkartlogo.png" alt="" /></NavLink></li>
-    <li><NavLink to={"https://blinkit.com/"}><img className="w-30 h-14" src="images/blinkit.svg" alt="" /></NavLink></li>
-    <li><a><img className="w-30 h-14" src="images/meesho.svg" alt="" /></a></li>
-  </ul>
-</div>
-          </NavLink>
-        </div>
-        <div className="flex flex-row justify-between w-auto gap-[30px] ">
-          <NavLink
-            to="https://www.flipkart.com/search?q=ypgold%20masala&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 ${
-                isActive ? "text-orange-700" : "text-black-900"
-              }  font-rubik text-4xl   lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-            }
-          >
-            <Img
-              src="images/img_list_black_900.svg"
-              alt="list_one"
-              className="h-6 w-6 "
-            />
-          </NavLink>
-          <NavLink
-            to="/B2B"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 ${
-                isActive ? "text-orange-700" : "text-black-900"
-              }  font-rubik text-4xl   lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-            }
-          >
->>>>>>> 90e30821550ac78069d2ff57a97c28e41011d7d1
 
               B2B
 
