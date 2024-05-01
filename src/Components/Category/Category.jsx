24b5367e459d923acb415d159c261adafb4f5682
Dineshwar420 <dineshwar420@gmail.com>
2024-05-01 20:35:16 +0530

    category bg-image added

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -14,13 +14,13 @@ function Category() {
     setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
   };
   const slides = [
-    { name: "Basic Spices",bg_image:"",link:"/Basic", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/basic.jpeg" },
-    { name: "Straight/Blend spices",bg_image:"", link:"/Blend", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/blend_1.jpeg" },
-    { name: "Mixed Spices",bg_image:"",link:"/Mixed", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/mixed.jpeg" },
-    { name: "Salts", bg_image:"",link:"/Salts",content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/salts.jpeg" },
-    { name: "Others",bg_image:"",link:"/Others", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/others.jpeg" },
-    { name: "Pooja Samagri",bg_image:"",link:"/Pooja", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/pooja.jpeg" },
-    { name: "Hing (asafoetida)",bg_image:"",link:"/Hing", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/hing_2.jpeg"},
+    { name: "Basic Spices", bg_image: "images/basic_cat.jpeg", link: "/Basic", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/basic.jpeg" },
+    { name: "Straight/Blend spices", bg_image: "images/blend_cat.jpeg", link: "/Blend", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/blend_1.jpeg" },
+    { name: "Mixed Spices", bg_image: "images/mixed_cat.jpeg", link: "/Mixed", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/mixed.jpeg" },
+    { name: "Salts", bg_image: "images/salt_cat.jpeg", link: "/Salts", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/salts.jpeg" },
+    { name: "Others", bg_image: "images/others_cat.jpeg", link: "/Others", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/others.jpeg" },
+    { name: "Pooja Samagri", bg_image: "", link: "/Pooja", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/pooja.jpeg" },
+    { name: "Hing (asafoetida)", bg_image: "", link: "/Hing", content: "YP Gold spices give you the best of taste, flavour and aroma as they are sourced from the prime spice growing regions. We do not go through the grounding process and retain most of their original qualities, they are cleaned using stringent processes to make them free from impurities. With this, we ensure that you receive only the best quality spices in India, guaranteed not only to be pure but also to delight your palate with their taste and aroma.", img: "images/hing_2.jpeg" },
   ];
   const scrollToTop = () => {
     window.scrollTo({
@@ -29,14 +29,14 @@ function Category() {
     });
   };
 
+
   return (
-    <div className='w-full h-[550px] bg-[#F0F0F0] max-[450px]:h-[600px]'>
-      <div className=" h-[32rem] overflow-hidden relative" style={{ backgroundImage: `url(${slides[currentSlide].bg_image})`, backgroundSize: 'cover', backgroundRepeat: '', mixblendmode:'multiply'}}>
+    <div className='w-full h-[650px]  bg-[#F0F0F0] max-[450px]:h-[600px]'>
+      <div className=" h-full overflow-hidden relative bg-center bg-cover mix-blend-multiply " style={{ backgroundImage: `url(${slides[currentSlide].bg_image})`, backgroundSize: 'cover', mixblendmode: 'multiply', transition: 'background-image 1s' }}>
         <div className="flex transition-transform duration-1000 ease-in-out" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
+
           {slides.map((slides, index) => (
-            <div
-              key={index} className="carousel-item relative w-full flex justify-center gap-8 flex-col md:flex-row max-[450px]:gap-0 " 
-            >
+            <div key={index} className=" carousel-item relative w-full flex justify-center gap-8 flex-col md:flex-row max-[450px]:gap-0 " >
               <div className="max-[450px]:w-full w-1/3 lg:mt-[4rem]   order-2 md:order-1 md:w-1/2 lg:w-1/3 md:mt-[0] p-4">
                 <div className="text-3xl font-bold max-[450px]:ml-20 ">
                   <h1 className='max-[450px]:text-black-900 max-[100px]:text-black-900'>{slides.name}</h1>
@@ -46,7 +46,7 @@ function Category() {
                     {slides.content}
                   </p>
                 </div>
-                <div className="flex items-center mt-3    ">
+                <div className="flex items-center mt-3  ">
                   <div className="dropdown dropdown-hover py-2 pr-4 pl-3 relative max-[450px]:dropup">
                     <button class="flex items-center bg-yellow-500 h-[60px] text-[white]  max-[450px]:pr-6  max-[450px]:border-solid border-solid max-[450px]:p-2 border-2  hover:bg-[#0097CC] hover:text-white-A700 p-2 w-auto">
                       Online order{" "}
@@ -92,16 +92,17 @@ function Category() {
                   </NavLink>
                 </div>
               </div>
-              <div className={`mt-[-1.5rem] bg-[#F0F0F0] order-1 md:order-2 md:w-1/2 lg:w-1/3 p-4 `}>
-                <img className={`h-[500px] w-[500px] mix-blend-multiply  max-[450px]:h-[200px] ` } src={slides.img} alt="" />
+              <div className={`mt-[-1.5rem]  order-1 md:order-2 md:w-1/2 lg:w-1/3 p-4 `}>
+                <img className={`h-[450px] w-[500px] mix-blend-multiply  max-[450px]:h-[200px] `} src={slides.img} alt="" />
               </div>
             </div>
-       
-        ))}
+
+          ))}
+        </div>
+
+        <button className=" hover:bg-[#0097CC] bg-[#F0F0F0] btn btn-circle absolute top-1/2 left-0 transform -translate-y-1/2 max-[450px]:hover:bg-[#0097CD] " onClick={prevSlide}> ❮</button>
+        <button className=" hover:bg-[#0097CC] btn btn-circle absolute top-1/2 right-0 transform -translate-y-1/2 max-[450px]:hover:bg-[#0097CD] max-[450px]:bg-[#F0F0F0]" onClick={nextSlide}>❯ </button>
       </div>
-      <button className=" hover:bg-[#0097CC] bg-[#F0F0F0] btn btn-circle absolute top-1/2 left-0 transform -translate-y-1/2 max-[450px]:hover:bg-[#0097CD] " onClick={prevSlide}> ❮</button>
-      <button className=" hover:bg-[#0097CC] btn btn-circle absolute top-1/2 right-0 transform -translate-y-1/2 max-[450px]:hover:bg-[#0097CD] max-[450px]:bg-[#F0F0F0]" onClick={nextSlide}>❯ </button>
-    </div>
     </div>
 
   );
