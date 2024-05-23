b8d7140b181dd90304fe16cf37f7cac4065ab679
JaiThakur10 <dev10jai@gmail.com>
2024-05-23 20:09:09 +0530

    link added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -46,7 +46,10 @@ export default function Footer({ ...props }) {
           </div>
         </div>
       </div>
-      <div className="flex  w-full mt-[50px] p-6 border-t-2 border-solid bg-[#0097CD] flex justify-end">
+      <div className="  w-full mt-[50px] p-6 border-t-2 border-solid bg-[#0097CD] flex justify-end">
+        <div className=" w-[500px] h-[10px] flex gap-4  justify-center text-white-A700" >
+          <h1> <a href="https://start-up-eight.vercel.app/">Created by Weularity</a>  </h1>
+        </div>
         <div className="  w-[500px] h-[10px] flex gap-4  justify-center text-white-A700">
           <h1 >Sitemap</h1> <h1>|</h1>
           <h1>Disclaimer</h1> <h1>|</h1>
